970ad5ac06ea1edf6dd05de11f4d7e77ab5a3d37
Nghia Nguyen <nguyencongnghia3004@gmail.com>
2026-09-01 19:16:24 -0500

    Finish the scaffolding code for the calculation layer

---
+++ script/calculation.js
@@ -0,0 +1,15 @@
+export function calculateRequiredHelium(){
+    //TODO : Implement to calculation in here
+}
+
+export function calcAvgBurstAlt(){
+    //TODO : Implement to calculation in here
+}
+
+export function calcAvgAscentRate(){
+    //TODO : Implement to calculation in here
+}
+
+export function calcAscentTime(){
+    //TODO : Implement to calculation in here
+}
